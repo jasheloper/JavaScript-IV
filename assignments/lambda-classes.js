525a9b0260773dbd70c7f57c335782995a54b9b6
Jashele T <jashelet@Jashele.domain>
2019-05-30 18:55:27 -0700

    added person class, instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,6 +2,31 @@
 
 class Person {
     constructor(attributes) {
-        
+        name = attributes.name; 
+        age = attributes.age;
+        location = attributes.location; 
+    }
+
+    speak() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+
+    demo() {
+        const subject = 'JavaScript';
+        return `Today we are learning about ${this.subject}.`
+    }
+
+    grade() {
+        const studentname = 'Jashele Tillman';
+        return `${this.studentname} receives a perfect score on ${this.subject}.`
     }
 }
